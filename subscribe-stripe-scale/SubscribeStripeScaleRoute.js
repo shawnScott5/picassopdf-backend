11eca9a7a0c0d57e752a288b13c498f330405109
sscott5 <sscott@quantumleafsolutions.com>
2025-03-15 19:55:24 -0400

    Another fix

--- subscribe-stripe-scale/SubscribeStripeScaleRoute.js
+++ subscribe-stripe-scale/SubscribeStripeScaleRoute.js
@@ -10,15 +10,15 @@ const SubscribeStripeScaleRoute = express.Router();
 
 // You can delegate this to the existing logic
 SubscribeStripeScaleRoute.post('/', async(req, res) => {
-    try {
+      try {
         const { priceId } = req.body; // Price ID from frontend
     
         const session = await stripe.checkout.sessions.create({
           payment_method_types: ["card"],
           mode: "subscription",
           line_items: [{ price: priceId, quantity: 1 }],
-          success_url: "https://app.distros.io/membership",
-          cancel_url: "https://app.distros.io/membership",
+          success_url: "https://app.distros.io/",
+          cancel_url: "https://app.distros.io/",
         });
     
         return res.json({ sessionUrl: session.url }); // Send back checkout URL to frontend
