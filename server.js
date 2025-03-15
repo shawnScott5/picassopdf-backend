ebcd6063184a3aba622670db941618072297147f
sscott5 <sscott@quantumleafsolutions.com>
2025-03-15 17:23:02 -0400

    route refresh fixed

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.use('/api/subscribe', SubscribeRoute);
 app.use(express.static(path.join(__dirname, '..', 'dist', 'distros-frontend')));
 
 // Handle all other routes by redirecting to `index.html`
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'dist', 'distros-frontend', 'index.html'));
 });
 
